ffa2e5bb1d2bca761fe66673784a1614f1ebd329
George Priniski <priniskigeorge@gmail.com>
2016-06-07 00:20:13 +0000

    Add list of courses

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -7,7 +7,11 @@ class Instructor::CoursesController < ApplicationController
 
   def create
     @course = current_user.courses.create(course_params)
-    redirect_to instructor_course_path(@course)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :uprocessable_entity
+    end    
   end
 
   def show
